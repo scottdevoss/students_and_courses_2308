3f96b864f239c4cdcfe09f96e493175b4ca65f44
Scott DeVoss <scottdevoss@yahoo.com>
2023-09-06 09:46:33 -0600

    feat: add iteration 2

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,21 @@
 class Course
-  
+  attr_reader :name, :capacity, :students, :full
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+    @full = false
+  end
+
+  def full?
+    if @students.length >= @capacity
+      @full = true
+    else 
+      @full = false
+    end
+  end
+
+  def enroll(student)
+    @students << student
+  end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,27 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do
-  
+  it 'exists' do
+    course = Course.new("Calculus", 2)
+    expect(course).to be_instance_of(Course)
+  end
+
+  it 'can be full' do
+    course = Course.new("Calculus", 2)
+    expect(course.full?).to be false
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+    course.enroll(student1)
+    course.enroll(student2)
+    expect(course.full?).to be true
+  end
+
+  it 'can enroll students' do
+    course = Course.new("Calculus", 2)
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+    course.enroll(student1)
+    course.enroll(student2)
+    expect(course.students.class).to eq(Array)
+  end
 end
