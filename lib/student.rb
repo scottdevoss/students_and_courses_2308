dc21ec295cb9c7e699c22f68319f1771c30e9dfd
Scott DeVoss <scottdevoss@yahoo.com>
2023-09-06 09:32:12 -0600

    add iteration 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
 class Student
-  
+  attr_reader :name, :age, :scores
+  def initialize(student_info)
+    @name = student_info[:name]
+    @age = student_info[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.inject {|sum, el| sum + el}.to_f / @scores.size
+  end
 end
